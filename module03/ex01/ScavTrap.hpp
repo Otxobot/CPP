3d046a069828cac201daefb65d2e829b9ccc2101
Otxobot <otxoboy64@gmail.com>
2024-02-01 11:22:33 +0100

    headers added

--- module03/ex01/ScavTrap.hpp
+++ module03/ex01/ScavTrap.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 11:21:34 by abasante          #+#    #+#             */
+/*   Updated: 2024/02/01 11:21:36 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP
 
@@ -7,7 +19,7 @@ class ScavTrap : public ClapTrap
 {
     public:
         ScavTrap(std::string name);
-        ScavTrap(ScatTrap const& obj);
+        ScavTrap(ScavTrap const& obj);
         ~ScavTrap();
         ScavTrap& operator=(ScavTrap const& rhs);
         void guardGate();
