278615690fe863a7bf0d64e17574632967bd43e6
Otxobot <otxoboy64@gmail.com>
2024-02-06 16:52:15 +0100

    empezando mod04

--- module03/ex02/main.cpp
+++ module03/ex02/main.cpp
@@ -6,37 +6,55 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 11:21:45 by abasante          #+#    #+#             */
-/*   Updated: 2024/02/06 13:19:12 by abasante         ###   ########.fr       */
+/*   Updated: 2024/02/06 13:23:16 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/**
- * @file main.cpp
- * @author abasante
- * @brief This file only contains the main function.
- * @version 0.1
- * @date 2024-02-02
- * 
- * @copyright Copyright (c) 2024
- * 
- */
-
-#include "ClapTrap.hpp"
-#include "ScavTrap.hpp"
-#include "FragTrap.hpp"
-
-/**
- * @brief Main function.
- * 
- * @param void
- * 
- * @return int 
- */
 
 //En C++, cuando un objeto de una clase derivada es destruido, 
 //primero se llama al destructor de la clase derivada y luego al destructor de la clase base. 
 //Esto es porque la creación de un objeto de una clase derivada implica primero construir el objeto base y luego construir el objeto derivado.
 //Por lo tanto, la destrucción ocurre en el orden inverso: primero se destruye el objeto derivado y luego el objeto base
+
+// C++ Implementation to show that a derived class
+// doesn’t inherit access to private data members.
+// However, it does inherit a full parent object.
+// class A {
+// public:
+//     int x;
+ 
+// protected:
+//     int y;
+ 
+// private:
+//     int z;
+// };
+ 
+// class B : public A {
+//     // x is public
+//     // y is protected
+//     // z is not accessible from B
+// };
+ 
+// class C : protected A {
+//     // x is protected
+//     // y is protected
+//     // z is not accessible from C
+// };
+ 
+// class D : private A // 'private' is default for classes
+// {
+//     // x is private
+//     // y is private
+//     // z is not accessible from D
+// };
+
+// public inheritance makes public members of the base class public in the derived class, and the protected members of the base class remain protected in the derived class.
+// protected inheritance makes the public and protected members of the base class protected in the derived class.
+// private inheritance makes the public and protected members of the base class private in the derived class.
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main(void)
 {
--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/01 11:21:34 by abasante          #+#    #+#             */
-/*   Updated: 2024/02/06 14:09:05 by abasante         ###   ########.fr       */
+/*   Created: 2024/02/06 16:49:32 by abasante          #+#    #+#             */
+/*   Updated: 2024/02/06 16:49:33 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAVTRAP_HPP
-#define SCAVTRAP_HPP
-
-#include "ClapTrap.hpp"
-
-class ScavTrap : public ClapTrap
-{
-    public:
-        ScavTrap();
-        ScavTrap(std::string name);
-        ScavTrap(ScavTrap const& obj);
-        ~ScavTrap();
-        ScavTrap& operator=(ScavTrap const& rhs);
-        void guardGate();
-        void attack(const std::string& target);
-};
-
-
-#endif
