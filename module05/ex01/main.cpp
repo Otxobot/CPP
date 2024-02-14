731d05be33bd75062ab0ac80fad075c675528017
Otxobot <otxoboy64@gmail.com>
2024-02-14 16:12:55 +0100

    empezando ex01

---
+++ module05/ex01/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 13:25:05 by abasante          #+#    #+#             */
+/*   Updated: 2024/02/14 16:04:12 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	try {
+    Bureaucrat bureaucrat("ash", 1);
+
+    std::cout << bureaucrat << std::endl;
+
+    bureaucrat.incrementGrade();
+        // bureaucrat.decrementGrade();
+    } catch (Bureaucrat::GradeTooHighException &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
