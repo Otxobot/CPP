06c29331ea4aa7ab989f4e7507f1fac369b4df59
Otxobot <otxoboy64@gmail.com>
2024-02-14 15:44:16 +0100

    ex00 hecho

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -6,13 +6,61 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 13:25:03 by abasante          #+#    #+#             */
-/*   Updated: 2024/02/14 13:30:35 by abasante         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:44:06 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(std::string &name, int grade)
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150) {
+	std::cout << "DEFAULT bureaucrat constructor called\n";
+}
+
+Bureaucrat::Bureaucrat(std::string &name, int grade) : _name(name) {
+	if (grade >= 150)
+		throw GradeTooLowException();
+	else if (grade <= 0)
+		throw GradeTooHighException();
+	this->_grade = grade;	
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& obj)
 {
-	
+	std::cout << "COPY constructor called\n";
+	*this = obj;	
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& rhs)
+{
+	this->_grade = rhs._grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "DESTRUCTOR bureaucrat called\n";
+}
+
+std::string Bureaucrat::getName() const
+{
+	return (this->_name);	
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
+
+void Bureaucrat::incrementGrade() 
+{
+	if (this->_grade <= 1)
+		throw GradeTooHighException();
+	this->_grade--;
+}
+
+void Bureaucrat::decrementGrade() 
+{
+	if (this->_grade >= 150)
+		throw GradeTooLowException();
+	this->_grade++;
 }
--- module05/ex00/Bureaucrat.hpp
+++ module05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 13:25:00 by abasante          #+#    #+#             */
-/*   Updated: 2024/02/14 13:31:49 by abasante         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:31:42 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ class Bureaucrat
 		const std::string _name;
 		int	_grade;
 	public:
+		Bureaucrat();
 		Bureaucrat(std::string& name, int grade);
 		Bureaucrat(const Bureaucrat& obj);
 		Bureaucrat& operator=( const Bureaucrat& rhs );
@@ -29,6 +30,28 @@ class Bureaucrat
 		std::string getName() const;
 		int getGrade() const;
 		
+		void	decrementGrade();
+		void	incrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too high");
+				}
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too low");
+				}
+		};
 };
+
+std::ostream& operator<<( std::ostream& o, const Bureaucrat& b );
 
 #endif
