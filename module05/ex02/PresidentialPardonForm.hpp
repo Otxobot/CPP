5c36054df3f81cec5b48bdfd53bc66ac4018185d
Otxobot <otxoboy64@gmail.com>
2024-02-19 17:15:22 +0100

    possiblemente hecho

--- module05/ex02/PresidentialPardonForm.hpp
+++ module05/ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,34 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 13:34:02 by abasante          #+#    #+#             */
-/*   Updated: 2024/02/19 13:34:03 by abasante         ###   ########.fr       */
+/*   Updated: 2024/02/19 17:13:38 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+# include <iostream>
+# include <fstream>
+# include "AForm.hpp"
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+class AForm;
+
+class PresidentialPardonForm : public AForm
+{
+    private:
+        std::string target;
+    public:
+        PresidentialPardonForm();
+        PresidentialPardonForm(std::string target);
+        PresidentialPardonForm(const PresidentialPardonForm& obj);
+		~PresidentialPardonForm();
+        PresidentialPardonForm &operator=(const PresidentialPardonForm& rhs);
+
+        std::string getTarget() const;
+		void execute(Bureaucrat const &executor) const;
+};
+
+#endif
