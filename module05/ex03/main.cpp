029fa4b93234d9717ef1ad90e0c17add6c04ef67
Otxobot <otxoboy64@gmail.com>
2024-02-21 13:42:59 +0100

    FINISHED

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 13:33:34 by abasante          #+#    #+#             */
-/*   Updated: 2024/02/21 11:19:31 by abasante         ###   ########.fr       */
+/*   Updated: 2024/02/21 13:41:41 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,20 @@
 
 int main()
 {
+    try{    
     Intern  someRandomIntern;
     AForm*   rrf;
 
-    rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+    rrf = someRandomIntern.makeForm("shrubberry creation", "Bender");
     std::cout << "name de rrf-> " << rrf->getName() << std::endl;
     delete rrf;
     std::cout << "====================================================================\n";
     rrf = someRandomIntern.makeForm("presidential pardon", "hello");
     std::cout << "name de rrf despues de otro makeform-----> " << rrf->getName() << std::endl;
     delete rrf;
+    } catch(std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
