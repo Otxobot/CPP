10d8f5fc840dd889e988a62caed484993bdf857a
Otxobot <otxoboy64@gmail.com>
2024-02-22 19:03:46 +0100

    empezando mod06

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -11,20 +11,54 @@
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+#include <exception>
 
-
-ScalarConvertor::ScalarConvertor(){
+ScalarConverter::ScalarConverter(){
 }
 
-ScalarConvertor::ScalarConvertor(ScalarConvertor const & src){
+ScalarConverter::ScalarConverter(ScalarConverter const & src){
 	*this = src;
 }
 
-ScalarConvertor::~ScalarConvertor(){
+ScalarConverter::~ScalarConverter(){
 }
 
-ScalarConvertor & ScalarConvertor::operator=(ScalarConvertor const & rhs){
+ScalarConverter & ScalarConverter::operator=(ScalarConverter const & rhs){
 	(void)rhs;
 	return *this;
 }
 
+void ScalarConverter::convert(const std::string &str)
+{
+	int i;
+	double d;
+	float f;
+	
+	try{
+		if (str == "nan" || str == "nanf" || str == "inff" || str == "inf" || str == "-inff" || str == "-inf")
+			throw std::exception();
+	}
+
+	if (std::isdigit(str[0]))
+	{
+			i = static_cast<int>(std::atoi(str.c_str()));
+			f = static_cast<float>(std::atof(str.c_str()));
+			d = std::atof(str.c_str());
+	}
+	else
+	{
+			i = static_cast<int>(str[0]);
+			f = static_cast<float>(i);
+			d = static_cast<double>(i);
+	}
+
+		char	c = static_cast<char>(i);
+		std::cout << "char: ";
+		if (i < 32 || i > 126)
+			std::cout << "Non displayable" << std::endl;
+		else
+			std::cout << "'" << c << "'" << std::endl;
+		std::cout << "int: " << i << std::endl;
+		std::cout << "float: " << f << "f" << std::endl;
+		std::cout << "double: " << d << std::endl;
+}
