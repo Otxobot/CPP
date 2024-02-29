8555923461e55c4966bae1c83bf1d764c99d8e63
Otxobot <otxoboy64@gmail.com>
2024-02-29 11:16:41 +0100

    ex00

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:45:15 by abasante          #+#    #+#             */
-/*   Updated: 2024/02/26 17:45:46 by abasante         ###   ########.fr       */
+/*   Updated: 2024/02/29 10:49:19 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,13 @@ void ScalarConverter::convert(const std::string &str)
 	char c;
 	
 	try{
-		if (str == "nan" || str == "nanf" || str == "inff" || str == "-inf" || str == "+inf" || str == "-inff" || str == "+inff" || str == "-inf")
+		if (str == "-inff" || str == "+inff" || str == "nanf" || str == "-inf" || str == "+inf"  || str == "nan")
 			throw std::exception();
 	if (std::isdigit(str[0]))
 	{
 			i = (std::atoi(str.c_str()));
 			f = static_cast<float>(std::atof(str.c_str()));
-			d = std::atof(str.c_str());
+			d = static_cast<double>(std::atof(str.c_str()));
 	}
 	else
 	{
@@ -54,9 +54,7 @@ void ScalarConverter::convert(const std::string &str)
 	c = static_cast<char>(i);
 	if (i < 32 || i > 126)
 	{
-		std::cout << "=========================\n";
 		std::cout << "char: no imprimible" << std::endl;
-		std::cout << "=========================\n";
 	}
 	else
 		std::cout << "char: " << "'" << c << "'" << std::endl;
