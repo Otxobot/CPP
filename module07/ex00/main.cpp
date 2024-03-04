d66fa30c1a36567a7924d223408f92152fa1f0b0
Otxobot <otxoboy64@gmail.com>
2024-03-04 13:13:44 +0100

    mod07 ex00

---
+++ module07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 12:57:22 by abasante          #+#    #+#             */
+/*   Updated: 2024/03/04 13:12:25 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) 
+{
+	int a = 2;
+	int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 12:57:16 by abasante          #+#    #+#             */
+/*   Updated: 2024/03/04 13:11:34 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &x, T &y)
+{
+	T tmp;
+
+	tmp = x;
+	x = y;
+	y = tmp;	
+}
+
+template <typename T>
+T min(T &x, T &y)
+{
+	if (x < y)
+		return (x);
+	return (y);
+}
+
+template <typename T>
+T max(T &x, T &y)
+{
+	if (x > y)
+		return (x);
+	return (y);
+}
+
+#endif
