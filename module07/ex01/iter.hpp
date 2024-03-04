f9dd08f68cf179405cfccd3fd2a50407e5fb3967
Otxobot <otxoboy64@gmail.com>
2024-03-04 14:02:10 +0100

    mod07 ex01

---
+++ module07/ex01/iter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 13:16:23 by abasante          #+#    #+#             */
+/*   Updated: 2024/03/04 13:43:59 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename T>
+void iter(T *array, int len, void(*func)(T&))
+{
+	for (int i = 0; i < len; i++)
+		func(array[i]);
+}
+
+#endif
