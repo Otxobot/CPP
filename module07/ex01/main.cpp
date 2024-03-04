307021eaf002cd450849098782d51ad2900cecfc
Otxobot <otxoboy64@gmail.com>
2024-03-04 15:04:54 +0100

    mod07 ex01 hecho

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:16:17 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/04 14:07:55 by abasante         ###   ########.fr       */
+/*   Updated: 2024/03/04 15:02:44 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@ int main(void)
 	int arr1[] = { 1, 2, 3, 4, 5 };
 
     std::cout << "arr1: " << std::endl;
-    ::iter(arr1, N, &print);
+    ::iter(arr1, N, print);
 
     double arr2[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
 
     std::cout << "\narr2: " << std::endl;
-    ::iter(arr2, N, &print);
+    ::iter(arr2, N, print);
 
     char arr3[] = { 'a', 'b', 'c', 'd', 'e' };
 
     std::cout << "\narr3: " << std::endl;
-    ::iter(arr3, N, &print);
+    ::iter(arr3, N, print);
 
     std::string arr4[] = { "one", "two", "three", "four", "five" };
 
     std::cout << "\narr4: " << std::endl;
-    ::iter(arr4, N, &print);
+    ::iter(arr4, N, print);
 
     return (0);	
 }
