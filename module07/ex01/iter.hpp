aca16f3e06faf7b22b62c8606b881333d1be3f2e
Otxobot <otxoboy64@gmail.com>
2024-03-04 14:12:33 +0100

    mod07 ex01

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:16:23 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/04 13:43:59 by abasante         ###   ########.fr       */
+/*   Updated: 2024/03/04 14:12:01 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ void iter(T *array, int len, void(*func)(T&))
 {
 	for (int i = 0; i < len; i++)
 		func(array[i]);
+}
+
+template<typename T>
+void print(T &i)
+{
+	std::cout << i << std::endl;
 }
 
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,13 +6,35 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:16:17 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/04 14:01:31 by abasante         ###   ########.fr       */
+/*   Updated: 2024/03/04 14:07:55 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+#define N 5
+
 int main(void)
 {
-	
+	int arr1[] = { 1, 2, 3, 4, 5 };
+
+    std::cout << "arr1: " << std::endl;
+    ::iter(arr1, N, &print);
+
+    double arr2[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+
+    std::cout << "\narr2: " << std::endl;
+    ::iter(arr2, N, &print);
+
+    char arr3[] = { 'a', 'b', 'c', 'd', 'e' };
+
+    std::cout << "\narr3: " << std::endl;
+    ::iter(arr3, N, &print);
+
+    std::string arr4[] = { "one", "two", "three", "four", "five" };
+
+    std::cout << "\narr4: " << std::endl;
+    ::iter(arr4, N, &print);
+
+    return (0);	
 }
