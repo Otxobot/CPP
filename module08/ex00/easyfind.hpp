03f2c07c18e6325275e03003a2342841aa90d90e
Otxobot <otxoboy64@gmail.com>
2024-03-07 16:32:05 +0100

    mod08 ex00 mas o menos

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -1,6 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 12:41:18 by abasante          #+#    #+#             */
+/*   Updated: 2024/03/07 16:30:17 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <deque>
 
+template <typename T>
+void easyfind(T& container, int number)
+{
+	if ( std::find( container.begin(), container.end(), number ) != container.end() )
+        std::cout << "Found." << std::endl;
+    else
+        std::cout << "Not found." << std::endl;	
+}
 
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -1,7 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 12:41:21 by abasante          #+#    #+#             */
+/*   Updated: 2024/03/07 16:31:06 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "easyfind.hpp"
 
 int main()
 {
+	int arr[] = {1, 2, 3, 4, 5};
 
+	std::cout << "Vector:" << std::endl;
+	std::vector<int>  vect(arr, arr + sizeof(arr) / sizeof(int));
+	easyfind(vect, 0);
+	std::cout << "================================" << std::endl;
+	std::cout << "List:" << std::endl;
+	std::list<int> list(arr, arr + sizeof(arr) / sizeof(int));
+	easyfind(list, 4);
+	std::cout << "================================" << std::endl;
+	std::cout << "Deque:" << std::endl;
+	std::deque<int> deque(arr, arr + sizeof(arr) / sizeof(int));
+	easyfind(deque, 5);
+
+	return 0;
 }
