d3de9da2ca29abfba8c6ddf684c3c0b2f85a78d3
Otxobot <otxoboy64@gmail.com>
2024-03-11 11:16:02 +0100

    mod08 ex01 empezando

--- module08/ex01/Span.cpp
+++ module08/ex01/Span.cpp
@@ -6,7 +6,22 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 16:37:59 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/07 16:38:00 by abasante         ###   ########.fr       */
+/*   Updated: 2024/03/11 11:11:10 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+class Span
+{
+	private:
+		unsigned int N;
+	public:
+		Span();
+		Span(unsigned int n) : N(n) {}
+		Span
+			
+};
+
+#endif
