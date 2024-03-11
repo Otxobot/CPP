196743eb6e3c045a347d4d8c21c41f091647a01d
Otxobot <otxoboy64@gmail.com>
2024-03-11 11:27:31 +0100

    mod08 ex01 empezand

--- module08/ex01/Span.cpp
+++ module08/ex01/Span.cpp
@@ -6,22 +6,9 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 16:37:59 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/11 11:11:10 by abasante         ###   ########.fr       */
+/*   Updated: 2024/03/11 11:27:00 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SPAN_HPP
-#define SPAN_HPP
+#include "Span.hpp"
 
-class Span
-{
-	private:
-		unsigned int N;
-	public:
-		Span();
-		Span(unsigned int n) : N(n) {}
-		Span
-			
-};
-
-#endif
--- module08/ex01/Span.hpp
+++ module08/ex01/Span.hpp
@@ -6,7 +6,29 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 16:38:01 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/07 16:38:02 by abasante         ###   ########.fr       */
+/*   Updated: 2024/03/11 11:26:51 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <deque>
+
+class Span
+{
+	private:
+		std::vector<int> _numbers;
+		unsigned int _N;
+	public:
+		Span();
+		Span(unsigned int n) : _N(n), _numbers() {}
+		Span(const Span& obj) : _N(obj._N), _numbers(obj._numbers) {}
+		
+};
+
+#endif
