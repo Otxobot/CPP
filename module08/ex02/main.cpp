8782afb619d5d3e45aea9758a81fc83e0d5dbc89
Otxobot <otxoboy64@gmail.com>
2024-03-18 17:16:37 +0100

    mod08 ex02 hecho

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 13:50:42 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/18 17:02:03 by abasante         ###   ########.fr       */
+/*   Updated: 2024/03/18 17:16:12 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 int main()
 {
+    std::cout << "=======================MutantStack=========================" << std::endl;
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
     std::cout << "top: " << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << "size: "<< mstack.size() << std::endl;
+    std::cout << "size: " << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
@@ -37,5 +38,28 @@ int main()
         ++it;
     }
     MutantStack<int> s(mstack);
+
+    std::cout << "=======================std::list=========================" << std::endl;
+
+    std::list<int> intList;
+		intList.push_back(5);
+		intList.push_back(17);
+		std::cout << "la ultima: " << intList.back() << std::endl;
+		intList.pop_back();
+		std::cout << "size: " <<intList.size() << std::endl;
+		intList.push_back(3);
+		intList.push_back(5);
+		intList.push_back(737);
+		//[...]
+		intList.push_back(0);
+		std::list<int>::iterator iterador1 = intList.begin();
+		std::list<int>::iterator iterador2 = intList.end();
+        int b = 0;
+		while (iterador1 != iterador2)
+		{
+            b++;
+			std::cout << b << "->" << *iterador1 << std::endl;
+			++iterador1;
+		}
     return (0);
 }
