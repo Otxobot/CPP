ea3fb94cb849c5517d48f6f9cde72e06a424beec
Otxobot <otxoboy64@gmail.com>
2024-04-01 12:57:40 +0200

    mod09 ex01 esta hecho

--- module09/ex01/main.cpp
+++ module09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 16:48:12 by abasante          #+#    #+#             */
-/*   Updated: 2024/03/29 16:48:14 by abasante         ###   ########.fr       */
+/*   Updated: 2024/04/01 12:53:46 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int ac, char **av)
 {
     if (ac != 2)
     {
-        std::cout << "Error: Minimo dos argumentos" << std::endl;
+        std::cout << "Error: Minimo y maximo dos argumentos" << std::endl;
         return (1);
     }
     RPN rpn(av[1]);
